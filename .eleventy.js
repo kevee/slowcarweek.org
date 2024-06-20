25a2a53c894c045758ae2c2c44b675c49dae0e1e
Kevin Miller <kevin@kevee.net>
2024-06-20 09:06:55 -0500

    minify html

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,22 @@
 const eleventySass = require('eleventy-sass')
+const htmlmin = require('html-minifier')
 
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPlugin(eleventySass)
   eleventyConfig.addWatchTarget('./src/sass/')
   eleventyConfig.addPassthroughCopy('./src/assets/images')
 
+  eleventyConfig.addTransform('htmlmin', (content, outputPath) => {
+    if (outputPath.endsWith('.html')) {
+      return htmlmin.minify(content, {
+        collapseWhitespace: true,
+        removeComments: true,
+        useShortDoctype: true,
+      })
+    }
+
+    return content
+  })
   return {
     dir: {
       input: 'src',
