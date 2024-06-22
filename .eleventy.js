140f62a147609caa14daf034f6468e2c33bd9c33
Kevin Miller <kevin@kevee.net>
2024-06-22 07:35:57 -0700

    add favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,12 @@
 const eleventySass = require('eleventy-sass')
 const htmlmin = require('html-minifier')
+const faviconPlugin = require('eleventy-favicon')
 
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPlugin(eleventySass)
   eleventyConfig.addWatchTarget('./src/sass/')
   eleventyConfig.addPassthroughCopy('./src/assets/images')
+  eleventyConfig.addPlugin(faviconPlugin, { destination: './public' })
 
   eleventyConfig.addTransform('htmlmin', (content, outputPath) => {
     if (outputPath.endsWith('.html')) {
